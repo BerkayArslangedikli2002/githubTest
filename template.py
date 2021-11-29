e2ccdd2f7e1e495113ba8f3a40f6872c44e7280d
BerkayArslangedikli2002 <berkayarslangedikli2002@gmail.com>
2021-11-29 18:29:58 +0100

    add the new template file

---
+++ template.py
@@ -0,0 +1,46 @@
+'''
+Filename: Template.py
+Purpose: 
+Date: 01/06/2021
+Author: Berkay Arslangedikli
+'''
+###########################################################
+### Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+###########################################################
+
+def function1(x):    
+    '''    
+    Purpose:        
+         
+    Inputs:    
+        x:  
+    Return value:        
+        y:    
+    '''    
+    
+def function2(x):    
+    '''    
+    Purpose:        
+        
+    Inputs:        
+        x: array     
+    no return value
+    '''    
+    print(x)
+    
+###########################################################
+def main():    
+    # Magic numbers    
+    x=np.array([1,2,3])
+    # Code 
+    function2(x)
+    y=function1(x)
+       
+    
+############################################################## 
+### call main
+if __name__ == "__main__":    
+    main()
